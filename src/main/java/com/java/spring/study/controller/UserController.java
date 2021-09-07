87e587c998fb3a2d64badff9272cf86b693984dc
wusubsin <mainmethod0126@gmail.com>
2021-09-07 22:28:12 +0900

    - @Service, @RestController, @Configuration, @Repository 등의 어노테이션을 등록하면 해당 클래스는 자동으로 Bean 으로 등록되면 Bean으 등록된 객체들을 @Autowired 로 주입받아 사용하기 위해서는 동일하게 Bean으로 등록된 클래스 내에서만 가능합니다.

--- src/main/java/com/java/spring/study/controller/UserController.java
+++ src/main/java/com/java/spring/study/controller/UserController.java
@@ -7,18 +7,15 @@ import com.java.spring.study.domain.User;
 import com.java.spring.study.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@ResponseStatus(value = HttpStatus.OK)
 public class UserController {
 
     @Autowired
@@ -31,6 +28,7 @@ public class UserController {
     public String test() {
         return "test";
     }
+
     @GetMapping("/api/user/all")
     public List<User> getUsers() {
         return service.getAllUsers();
